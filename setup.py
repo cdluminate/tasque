5dc17e8db3c661274e8b2049e34da07dafd67f58
Mo Zhou <cdluminate@gmail.com>
2018-04-25 16:24:51 +0000

    Import updated tq and bump version to 0.4.2

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='tq1',
-      version='0.4.1',
+      version='0.4.2',
       description='Simple Command Line Job Manager',
       long_description=open('./README.md', 'r').read(),
       long_description_content_type='text/markdown',
       url='https://github.com/CDLuminate/tq',
       author='Mo Zhou',
       author_email='cdluminate@gmail.com',
+      license='MIT',
       packages=['tq'],
       entry_points={'console_scripts': ['tq=tq:main']},
       )
